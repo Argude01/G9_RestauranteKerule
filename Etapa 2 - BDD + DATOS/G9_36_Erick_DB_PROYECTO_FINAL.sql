cf238166df78e7f6ef35d374647426b33bdd10e2
David10DX <79998512+David10DX@users.noreply.github.com>
2021-06-10 17:14:41 -0600

    Add files via upload

---
+++ Etapa 2 - BDD + DATOS/G9_36_Erick_DB_PROYECTO_FINAL.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-06-2021 a las 01:08:51
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_restaurante`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_clientes`
+--
+
+CREATE TABLE `tbl_clientes` (
+  `ID_CLIENTE` int(11) NOT NULL,
+  `NOMBRE` varchar(100) NOT NULL,
+  `APELLIDO` varchar(100) NOT NULL,
+  `TELEFONO` varchar(20) NOT NULL,
+  `DIRECCION` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tbl_clientes`
+--
+
+INSERT INTO `tbl_clientes` (`ID_CLIENTE`, `NOMBRE`, `APELLIDO`, `TELEFONO`, `DIRECCION`) VALUES
+(6, 'Erick David ', 'Blanco Santos', '96036528', 'col. Los Arcos'),
+(7, 'Henry Vector', 'Morado Rodríguez', '84879845', 'Cerro de Nance'),
+(8, 'María Jose', 'Ortega', '93874129', 'Barrio Arriba'),
+(9, 'Waldin Francisco', 'Castillo Montes', '93726509', 'col la maritima'),
+(10, 'Ricardo', 'Tovez Palomo', '76809765', 'Barrio La Mariposa'),
+(11, 'Sergi', 'Roberto', '98991001', 'Col. Jasminez');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_clientes`
+--
+ALTER TABLE `tbl_clientes`
+  ADD PRIMARY KEY (`ID_CLIENTE`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_clientes`
+--
+ALTER TABLE `tbl_clientes`
+  MODIFY `ID_CLIENTE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
